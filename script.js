a60fcb80e04118b544acaccd53f05d36ba31734c
Mohamed <abdi2k24@gmail.com>
2023-01-01 17:39:03 -0600

    added js logic var

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-var APIKey = "";
+var APIKey = "appid=9fa92ea9dac0de419dc72b07b352638a";
+var citiesListArr = [];
+var numOfCities = 5;
+var unit = "units=imperial";
+var dailyWeatherApiStarts =
+  "https://api.openweathermap.org/data/2.5/weather?q=";
+var dailyUVIndexApiStarts = "https://api.openweathermap.org/data/2.5/uvi?";
+var forecastWeatherApiStarts =
+  "https://api.openweathermap.org/data/2.5/onecall?";
+var searchCityForm = $("#searchCityForm");
+var searchedCities = $("#searchedCityLi");
