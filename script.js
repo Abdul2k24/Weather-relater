1b932f5233c6cfed8a39535fbb3f34f58946053b
Mohamed <abdi2k24@gmail.com>
2023-01-01 19:43:16 -0600

    js logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var APIKey = "appid=9fa92ea9dac0de419dc72b07b352638a";
 var citiesListArr = [];
-var numOfCities = 5;
+var numOfCities = 11;
+var personalAPIKey = "appid=264dbcaa3899de05eeadc78d68ba06dc";
 var unit = "units=imperial";
 var dailyWeatherApiStarts =
   "https://api.openweathermap.org/data/2.5/weather?q=";
@@ -9,3 +9,31 @@ var forecastWeatherApiStarts =
   "https://api.openweathermap.org/data/2.5/onecall?";
 var searchCityForm = $("#searchCityForm");
 var searchedCities = $("#searchedCityLi");
+var getCityWeather = function (searchCityName) {
+  var apiUrl =
+    dailyWeatherApiStarts + searchCityName + "&" + personalAPIKey + "&" + unit;
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      return response.json().then(function (response) {
+        $("#cityName").html(response.name);
+        var unixTime = response.dt;
+        var date = moment.unix(unixTime).format("MM/DD/YY");
+        $("#currentdate").html(date);
+        var weatherIncoUrl =
+          "http://openweathermap.org/img/wn/" +
+          response.weather[0].icon +
+          "@2x.png";
+        $("#weatherIconToday").attr("src", weatherIncoUrl);
+        $("#tempToday").html(response.main.temp + " \u00B0F");
+        $("#humidityToday").html(response.main.humidity + " %");
+        $("#windSpeedToday").html(response.wind.speed + " MPH");
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        getUVIndex(lat, lon);
+        getForecast(lat, lon);
+      });
+    } else {
+      alert("Please provide a valid city name.");
+    }
+  });
+};
